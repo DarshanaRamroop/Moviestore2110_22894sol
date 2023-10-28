553daf613f9b5a2cbc07fc1472e83105637addb3
yanya <yanya@HISOKA>
2023-10-28 15:25:16 +0400

    clear message

---
+++ Moviestore2110_22894/tutorials/week2/clearmessagebutton.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Moviestore2110_22894.tutorials.week2
+{
+    public partial class clearmessagebutton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (Request.Form["__EVENTTARGET"] == Button1.UniqueID)
+                {
+                    
+                    Button1.Text = string.Empty;
+                }
+            }
+
+        }
+    }
+}
