e734d9af9eb0248bad32e36840d90075250fa429
yanya <yanya@HISOKA>
2023-10-28 16:39:13 +0400

    task3

---
+++ Moviestore2110_22894/tutorials/week2/task3.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Moviestore2110_22894.tutorials.week2
+{
+    public partial class task3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                TextBox1.Text = TextBox2.Text;
+            }
+
+        }
+    }
+}
