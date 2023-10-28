a8badbae1935f5647a6c137c09907ba98e9ce3a6
yanya <yanya@HISOKA>
2023-10-28 17:51:39 +0400

    task5

---
+++ Moviestore2110_22894/tutorials/week2/task5.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Moviestore2110_22894.tutorials.week2
+{
+    public partial class task5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int thetime = DateTime.Now;
+
+            if (thetime < 12)
+            {
+                HyperLink1.Text = "Visit Morning Session";
+            }
+            else
+            {
+                HyperLink1.Text="Visit Afternoon Session";
+            }
+        }
+    }
+}
