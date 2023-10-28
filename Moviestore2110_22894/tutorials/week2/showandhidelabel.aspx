63609b209b31ee0ae211f836b81a59c6cdf25094
yanya <yanya@HISOKA>
2023-10-28 16:15:09 +0400

    task2

---
+++ Moviestore2110_22894/tutorials/week2/showandhidelabel.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/guestmasterpage.Master" AutoEventWireup="true" CodeBehind="showandhidelabel.aspx.cs" Inherits="Moviestore2110_22894.tutorials.week2.showandhidelabel" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+<asp:Button ID="Button1" runat="server" Text="Button" />
+<asp:Button ID="Button2" runat="server" Text="Button" />
---
+++ Moviestore2110_22894/tutorials/week2/showandhidelabel.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Moviestore2110_22894.tutorials.week2
+{
+    public partial class showandhidelabel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Label1.Visible = false;
+
+            }
+            else { 
+                Label1.Visible = !Label1.Visible; }
+
+        }
+    }
+
+
+}
