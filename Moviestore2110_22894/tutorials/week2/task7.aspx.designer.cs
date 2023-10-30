54e6f7a08df823ca753f159182a402b4ada8a907
yanya <yanya@HISOKA>
2023-10-30 12:59:38 +0400

    main

--- Moviestore2110_22894/tutorials/week2/task7.aspx.designer.cs
+++ Moviestore2110_22894/tutorials/week2/task7.aspx.designer.cs
@@ -96,6 +96,15 @@ namespace Moviestore2110_22894.tutorials.week2
         protected global::System.Web.UI.WebControls.Button Button8;
 
         /// <summary>
+        /// Button9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button9;
+
+        /// <summary>
         /// Button10 control.
         /// </summary>
         /// <remarks>
